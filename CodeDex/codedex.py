142636e94f9045f082aba6fce0a5778061aa5316
Christina Yakova <f109722@students.nbu.bg>
2025-01-27 18:27:02 +0200

    Add files via upload

---
+++ CodeDex/codedex.py
@@ -0,0 +1,43 @@
+import random
+
+print('''1) “✊” (Rock). 
+2) “✋” (Paper). 
+3) “✌️” (Scissors).''')
+player = int(input('Choose an option 1-3:'))
+computer = random.randint(1,3)
+
+if player == 1:
+    print('Player chooses: ✊')
+elif player == 2:
+    print('Player chooses: ✋')
+else:
+    print('Player chooses: ✌️')
+
+if computer == 1:
+    print('Computer chooses: ✊')
+elif computer == 2:
+    print('Computer chooses: ✋')
+else:
+    print('Computer chooses: ✌️')
+
+if player == 1:
+    if player == 1 and computer == 1:
+        print("It's a tie!")
+    elif player == 1 and computer == 2:
+        print("The computer wins!")
+    elif player == 1 and computer == 3:
+        print("The player wins!")
+elif player == 2:
+    if player == 2 and computer == 1:
+        print("The player wins!")
+    elif player == 2 and computer == 2:
+        print("It's a tie!")
+    elif player == 2 and computer == 3:
+        print("The computer wins!")
+elif player == 3:
+    if player == 3 and computer == 1:
+        print("The computer wins!")
+    elif player == 3 and computer == 2:
+        print("The player wins!")
+    elif player == 3 and computer == 3:
+        print("It's a tie!")
